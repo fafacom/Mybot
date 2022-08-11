5ec3662a34f8040107d0b91eecf55288a0ef39cf
fafacom <111061792+fafacom@users.noreply.github.com>
2022-08-11 19:47:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,10 +36,10 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6282162837259'] //ur owner number
+global.ownername = "Zuki" //ur owner name
+global.ytname = "YT: "Zuki" //ur yt chanel name
+global.socialm = "GitHub: DGZuki" //ur github or insta name
+global.location = "Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['6282162837259']
+global.ownertag = '6282162837259' //ur tag number
+global.botname = 'Mybot Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "mybot Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄Jangan diambil" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,59 +79,61 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
-global.ntilinkytvid = []
-global.ntilinkytch = []
-global.ntilinkig = []
-global.ntilinkfb = []
-global.ntilinktg = []
-global.ntilinktt = []
-global.ntilinktwt = []
-global.ntilinkall = []
-global.nticall = []
-global.ntwame = []
-global.nttoxic = []
+global.premium = ['6282162837259'] //ur premium numbers
+global.antilinkytvid = []
+global.antilinkytch = []
+global.antilinkig = []
+global.antilinkfb = []
+global.antilinktg = []
+global.antilinktt = []
+global.antilinktwt = []
+global.antilinkall = []
+global.anticall = []
+global.antwame = []
+global.antitoxic = []
 global.ntnsfw = []
-global.ntvirtex = []
+global.antvirtex = []
 global.rkyt = []
-global.wlcm = []
+global.welcome = []
 global.gcrevoke = []
 global.autorep = []
-global.ntilink = []
+global.antilink = []
 
 //omther
-global.sessionName = 'session'
+global.sessionName = 'SUCSESS'
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
+    success: 'SUCSESS ✓',
+    admin: 'Fitur Ini Hanya Untuk Admin!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!'
+',
+    owner: 'Fitur Ini Hanya Untuk Owner!',
+    group: 'Fitur Hanya Digunakan Untuk Grup!
+',
+    private: 'Fitur Hanya Digunakan Untuk Obrolan Pribadi!',
+    bot: 'Fitur Hanya digunakan Bot',
+    wait: 'Sedang prosess..',
+    linkm: 'Mana linknya ktl?',
+    error: 'Kesalahan!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
     banChat: 'The bot was banned in this group, please contact the owner to unban'
 }
     global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
+    premium: "no limit"
+    free: no limit,
+    monayawal: {
 }
    global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+   darahawal: no limit,
+   besiawal: no limit,
+   goldawal: no limit,
+   emeraldawal: no limit,
+   umpanawal: no limit,
+   potionawal: no limit
 }
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
